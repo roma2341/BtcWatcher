e1f187f13169797ce8d5d90c6fc6059a0f3a8128
Roman Zinchuk <max@softhousegroup.com>
2018-09-04 09:46:25 +0300

    get user, get wallets, get operations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+var BitGoJS = require('bitgo');
+var ACCESS_TOKEN = 'v2x0c637bfb5e4f1f69cd9104e2aedd97389354fb53f10d7e7073a8e6931dec97d1';
+var bitgo = new BitGoJS.BitGo({ env: 'test', accessToken: ACCESS_TOKEN });
+
 const http = require('http')
 const port = 3000
 const requestHandler = (request, response) => {
-    console.log(request.url)
+    console.log(request.url);
+    response.write('test');
+    //getUser();
+    getWallets();
+    getCoins();
     response.end('Hello Node.js Server!')
+
 }
 const server = http.createServer(requestHandler)
 server.listen(port, (err) => {
@@ -11,3 +20,39 @@ server.listen(port, (err) => {
     }
     console.log(`server is listening on ${port}`)
 })
+
+ function getUser(){
+    bitgo.me({}, function callback(err, user) {
+        if (err) {
+          // handle error
+        }
+       // console.log('user:'+JSON.stringify(user));
+      });  
+}
+
+function getCoins(){
+        bitgo.coin('tbtc').wallets().list({})
+        .then(function(wallets) {
+        // print the wallets
+        //console.dir(wallets);
+        });
+}
+
+function getWallets(){
+    bitgo.coin('tbtc').wallets().list({})
+.then(function(walletsResponse) {
+  // print the wallets
+  walletOperations(walletsResponse.wallets);
+  //console.dir(wallets);
+});
+}
+
+function walletOperations(wallets){
+    for(wallet of wallets){
+        wallet.transfers()
+        .then(function(transfers) {
+        // print transfers
+        console.dir(transfers);
+        });
+    }
+}
